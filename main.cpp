c6e520f95dd2555bf780d7f1dd3f19fbe94a8c7b
shalaga44 <mohamedmoawia4@gmail.com>
2020-03-05 10:51:23 +0200

    add make files

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//main.cpp
+#include "stats.h"
+#include "p1_library.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "hello from main!\n";
+}
---
+++ stats.cpp
@@ -0,0 +1,45 @@
+// stats.cpp
+#include "stats.h"
+#include <cassert>
+#include <vector>
+using namespace std;
+
+vector<vector<double> > summarize(vector<double> v) {
+  assert(false);
+}
+
+int count(vector<double> v) {
+  assert(false);
+}
+
+double sum(vector<double> v) {
+  assert(false);
+}
+
+double mean(vector<double> v) {
+  assert(false);
+}
+
+double median(vector<double> v) {
+  assert(false);
+}
+
+double mode(vector<double> v) {
+  assert(false);
+}
+
+double min(vector<double> v) {
+  assert(false);
+}
+
+double max(vector<double> v) {
+  assert(false);
+}
+
+double stdev(vector<double> v) {
+  assert(false);
+}
+
+double percentile(vector<double> v, double p) {
+  assert(false);
+}
